e030a9869a974d5450f079b5875a2bbe85df2d1f
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-09-11 16:06:57 +0500

    feat: build logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/pages/background/background.ts'),
+        content: resolve(__dirname, 'src/pages/content/content.ts'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
